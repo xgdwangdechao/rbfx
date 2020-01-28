057c22d2b90ea6fd6b22f32ef5d24a8df184d526
Rokas Kupstys <rokups@zoho.com>
2020-01-28 17:49:47 +0200

    CMake: Add URHO3D_MOBILE_GRAPHICS option to force mobile graphics profile.
    Resolves #182.

--- Source/Urho3D/Graphics/GraphicsDefs.h
+++ Source/Urho3D/Graphics/GraphicsDefs.h
@@ -33,7 +33,7 @@ namespace Urho3D
 class Vector3;
 
 /// Graphics capability support level. Web platform (Emscripten) also uses OpenGL ES, but is considered a desktop platform capability-wise
-#if defined(IOS) || defined(TVOS) || defined(__ANDROID__) || defined(__arm__) || defined(__aarch64__)
+#if defined(IOS) || defined(TVOS) || defined(__ANDROID__) || defined(__arm__) || defined(__aarch64__) || defined(URHO3D_MOBILE_GRAPHICS)
 #define MOBILE_GRAPHICS
 #else
 #define DESKTOP_GRAPHICS
