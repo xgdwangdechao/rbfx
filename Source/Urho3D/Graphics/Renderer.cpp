ac6d6ee196018749762e47d6901657985a790127
Eugene Kozlov <nekoeuge@gmail.com>
2020-07-25 18:15:50 +0300

    Add basic shadows.

--- Source/Urho3D/Graphics/Renderer.cpp
+++ Source/Urho3D/Graphics/Renderer.cpp
@@ -1599,13 +1599,13 @@ void Renderer::UpdateQueuedViewport(unsigned index)
     }
 
     // Update view. This may queue further views. View will send update begin/end events once its state is set
+    ResetShadowMapAllocations(); // Each view can reuse the same shadow maps
     if (customView)
     {
         customView->Update(frame_);
     }
     else
     {
-        ResetShadowMapAllocations(); // Each view can reuse the same shadow maps
         view->Update(frame_);
     }
 }
